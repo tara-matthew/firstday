b09ef1be09af474795ccc27edb446539935fd840
admin <tommaso.vallicr@gmail.com>
2019-08-01 20:56:30 +0100

    [VP] Activated plugin 'Custom Registration'
    VP-Action: plugin/activate/custom-registration-fom/custom-registration-form.php
    VP-Plugin-Name: Custom Registration
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: my-machine

---
+++ wp-content/plugins/custom-registration-fom/custom-registration-form.php
@@ -0,0 +1,78 @@
+<?php
+/*
+  Plugin Name: Custom Registration
+  Plugin URI: http://code.tutsplus.com
+  Description: Updates user rating based on number of posts.
+  Version: 1.0
+  Author: Agbonghama Collins
+  Author URI: http://tech4sky.com
+ */
+
+function registration_form( $username, $password, $email, $website, $first_name, $last_name, $nickname, $bio ) {
+    echo '
+    <style>
+    div {
+        margin-bottom:2px;
+    }
+     
+    input{
+        margin-bottom:4px;
+    }
+    </style>
+    ';
+
+    echo '
+    <form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
+    <div>
+    <label for="username">Username <strong>*</strong></label>
+    <input type="text" name="username" value="' . ( isset( $_POST['username'] ) ? $username : null ) . '">
+    </div>
+     
+    <div>
+    <label for="password">Password <strong>*</strong></label>
+    <input type="password" name="password" value="' . ( isset( $_POST['password'] ) ? $password : null ) . '">
+    </div>
+     
+    <div>
+    <label for="email">Email <strong>*</strong></label>
+    <input type="text" name="email" value="' . ( isset( $_POST['email']) ? $email : null ) . '">
+    </div>
+     
+    <div>
+    <label for="website">Website</label>
+    <input type="text" name="website" value="' . ( isset( $_POST['website']) ? $website : null ) . '">
+    </div>
+     
+    <div>
+    <label for="firstname">First Name</label>
+    <input type="text" name="fname" value="' . ( isset( $_POST['fname']) ? $first_name : null ) . '">
+    </div>
+     
+    <div>
+    <label for="website">Last Name</label>
+    <input type="text" name="lname" value="' . ( isset( $_POST['lname']) ? $last_name : null ) . '">
+    </div>
+     
+    <div>
+    <label for="nickname">Nickname</label>
+    <input type="text" name="nickname" value="' . ( isset( $_POST['nickname']) ? $nickname : null ) . '">
+    </div>
+     
+    <div>
+    <label for="bio">About / Bio</label>
+    <textarea name="bio">' . ( isset( $_POST['bio']) ? $bio : null ) . '</textarea>
+    </div>
+    <input type="submit" name="submit" value="Register"/>
+    </form>
+    ';
+}
+
+// Register a new shortcode: [cr_custom_registration]
+add_shortcode( 'cr_custom_registration', 'custom_registration_shortcode' );
+
+// The callback function that will replace [book]
+function custom_registration_shortcode() {
+    ob_start();
+    custom_registration_function();
+    return ob_get_clean();
+}
