93370953ab5a7adec18d78bcc00c33411a2b89b6
admin <tommaso.vallicr@gmail.com>
2019-08-01 11:29:21 +0100

    [VP] Activated plugin 'Add Textttt To Footer'
    VP-Action: plugin/activate/testPlugin.php
    VP-Plugin-Name: Add Textttt To Footer
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: my-machine

--- wp-content/plugins/testPlugin.php
+++ wp-content/plugins/testPlugin.php
@@ -1,7 +1,21 @@
-<?php
+
 /**
  * Created by PhpStorm.
  * User: taram
  * Date: 01/08/2019
  * Time: 11:25
  */
+
+<?php
+/*
+Plugin Name: Add Textttt To Footer
+*/
+
+// Hook the 'wp_footer' action hook, add the function named 'mfp_Add_Text' to it
+add_action("wp_footer", "mfp_Add_Text");
+
+// Define 'mfp_Add_Text'
+function mfp_Add_Text()
+{
+    echo "<p style='color: black;'>After the footer is loaded, my text is added!</p>";
+}
