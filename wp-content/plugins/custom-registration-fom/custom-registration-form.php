697b3fb875812362b51bb36978279c84de386d5f
Tara Matthew <taramatthew.93@gmail.com>
2019-08-07 22:59:40 +0100

    [VP] Deactivated plugin 'Custom Registration'
    VP-Action: plugin/deactivate/custom-registration-fom/custom-registration-form.php
    VP-Plugin-Name: Custom Registration
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: my-machine

--- wp-content/plugins/custom-registration-fom/custom-registration-form.php
+++ wp-content/plugins/custom-registration-fom/custom-registration-form.php
@@ -8,68 +8,154 @@
   Author URI: http://tech4sky.com
  */
 
-function registration_form($firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword) {
-    echo '
-    <style>
-    div {
-        margin-bottom:2px;
-    }
-     
-    input{
-        margin-bottom:4px;
-    }
-    </style>
-    ';
 
-    echo '
-    <form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
-    <div>
-    <label for="first-name">Nome<strong>*</strong></label>
-    <input type="text" name="first-name" value="' . ( isset( $_POST['first-name'] ) ? $firstName : null ) . '">
-    </div>
-    
-    <div>
-    <label for="last-name">Cognome<strong>*</strong></label>
-    <input type="text" name="last-name" value="' . ( isset( $_POST['last-name'] ) ? $lastName : null ) . '">
-    </div>
-    
-    <div>
-    <label for="school">Scuola<strong>*</strong></label>
-    <input type="text" name="school" value="' . ( isset( $_POST['school'] ) ? $school : null ) . '">
-    </div>
-    
-    <div>
-    <label for="course">Corso studi<strong>*</strong></label>
-    <input type="text" name="course" value="' . ( isset( $_POST['course'] ) ? $course : null ) . '">
-    </div>
-    
-    <div>
-    <label for="graduation-year">Anno conseguimento titolo<strong></strong></label>
-    <input type="text" name="graduation-year" value="' . ( isset( $_POST['graduation-year'] ) ? $graduationYear : null ) . '">
-    </div>
-    
-        
-    <div>
-    <label for="email">Email<strong>*</strong></label>
-    <input type="text" name="email" value="' . ( isset( $_POST['email'] ) ? $email : null ) . '">
-    </div>
-    
-    <div>
-    <label for="password">Password<strong>*</strong></label>
-    <input type="text" name="password" value="' . ( isset( $_POST['password'] ) ? $password : null ) . '">
-    </div>
-    
-    <div>
-    <label for="confirm-password">Conferma password<strong>*</strong></label>
-    <input type="text" name="confirm-password" value="' . ( isset( $_POST['confirm-password'] ) ? $confirmPassword : null ) . '">
-    </div>
-    
-        
-    <input type="submit" name="submit" value="Registrati"/>
+
+function student_registration_form_fields() {
+    ob_start(); ?>
+
+    <form action="<?=$_SERVER['REQUEST_URI']?>" id="student-registration" method="post">
+        <fieldset>
+            <div>
+                <label for="first-name">Nome<strong>*</strong></label>
+                <input type="text" name="first-name">
+             </div>
+
+            <div>
+                <label for="last-name">Cognome<strong>*</strong></label>
+                <input type="text" name="last-name">
+            </div>
+
+            <div>
+                <label for="school">Scuola<strong>*</strong></label>
+                <input type="text" name="school">
+            </div>
+
+            <div>
+                <label for="course">Corso studi<strong>*</strong></label>
+                <input type="text" name="course">
+            </div>
+
+            <div>
+                <label for="graduation-year">Anno conseguimento titolo<strong></strong></label>
+                <input type="text" name="graduation-year">
+            </div>
+
+            <div>
+                <label for="email">Email<strong>*</strong></label>
+                <input type="text" name="email">
+            </div>
+
+            <div>
+                <label for="password">Password<strong>*</strong></label>
+                <input type="text" name="password">
+            </div>
+
+            <div>
+                <label for="confirm-password">Conferma password<strong>*</strong></label>
+                <input type="text" name="confirm-password">
+            </div>
+
+            <div>
+                 <input type="submit" name="submit" value="Registrati"/>
+            </div>
+        </fieldset>
     </form>
-    ';
+
+    <?php return ob_get_clean();
 }
 
+function show_student_registration_form() {
+    $output = student_registration_form_fields();
+
+    return $output;
+}
+
+add_shortcode('student-registration-form', 'show_student_registration_form');
+add_action('init','add_new_student_user');
+
+function add_new_student_user() {
+
+    if (isset($_POST['first-name'])) {
+        if (wp_verify_nonce($_POST['register-nonce'], 'register-nonce')) {
+            $user_login             = $_POST['first-name'] . '' . $_POST['last-name'];
+            $user_email             = $_POST['email'];
+            $user_first             = $_POST['first-name'];
+            $user_last              = $_POST['last-name'];
+            $user_pass              = $_POST['password'];
+            $pass_confirm           = $_POST['confirm-password'];
+            $school                 = $_POST['school'];
+            $course                 = $_POST['course'];
+            $graduation_year        = $_POST['graduation-year'];
+
+            //error handling here
+
+            $new_user_ID = wp_insert_user(array(
+                'user_login'        => $user_login,
+                'user_email'        => $user_email,
+                'user_pass'         => $user_pass,
+                'first_name'    	=> $user_first,
+                'last_name'		    => $user_last,
+                'user_registered'	=> date('Y-m-d H:i:s'),
+                'role'				=> 'subscriber',
+            ));
+
+            $metas = array(
+                'type'              => 'student',
+                'school'            => $school,
+                'course'            => $course,
+                'graduation_year'   => $graduation_year
+            );
+
+            foreach ($metas as $key => $value) {
+                update_user_meta($new_user_ID, $key, $value);
+            }
+
+            //echo $new_user_ID; die();
+
+
+        } else {
+            echo 'failed'; die();
+        }
+    }
+
+}
+
+
+
+
+/*function complete_registration() {
+    global $reg_errors, $firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword;
+
+    if ( 1 > count( $reg_errors->get_error_messages() ) ) {
+        $userdata = array(
+            'user_login'    => $firstName . '' . $lastName,
+            'user_email'    =>   $email,
+            'user_pass'     =>   $password,
+            'first_name'    =>   $firstName,
+            'last_name'     =>   $lastName,
+        );
+        $userID = wp_insert_user( $userdata );
+
+        $metas = array(
+          'type' => 'student',
+          'school' => $school,
+          'course' => $course,
+          'graduation_year' => $graduationYear
+        );
+
+
+        // Put an update condition in here too, in case the user exists?
+        foreach ($metas as $key => $value) {
+            update_user_meta($userID, $key, $value);
+        }
+        //$userMeta = add_user_meta($user->ID, 'type', 'student');
+        //var_dump($user); die();
+        //echo 'Registration complete. Goto <a href="' . get_site_url() . '/wp-login.php">login page</a>.';
+    }
+}
+
+// Register a new shortcode: [cr_custom_registration]
+add_shortcode( 'cr_custom_registration', 'custom_registration_shortcode' );
 
 function registration_validation($firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword) {
 
@@ -157,43 +243,9 @@ function custom_registration_function() {
     );
 }
 
-function complete_registration() {
-    global $reg_errors, $firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword;
-
-    if ( 1 > count( $reg_errors->get_error_messages() ) ) {
-        $userdata = array(
-            'user_login'    => $firstName . '' . $lastName,
-            'user_email'    =>   $email,
-            'user_pass'     =>   $password,
-            'first_name'    =>   $firstName,
-            'last_name'     =>   $lastName,
-        );
-        $userID = wp_insert_user( $userdata );
-
-        $metas = array(
-          'type' => 'student',
-          'school' => $school,
-          'course' => $course,
-          'graduation_year' => $graduationYear
-        );
-
-
-        // Put an update condition in here too, in case the user exists?
-        foreach ($metas as $key => $value) {
-            update_user_meta($userID, $key, $value);
-        }
-        //$userMeta = add_user_meta($user->ID, 'type', 'student');
-        //var_dump($user); die();
-        echo 'Registration complete. Goto <a href="' . get_site_url() . '/wp-login.php">login page</a>.';
-    }
-}
-
-// Register a new shortcode: [cr_custom_registration]
-add_shortcode( 'cr_custom_registration', 'custom_registration_shortcode' );
-
 // The callback function that will replace [book]
 function custom_registration_shortcode() {
     ob_start();
     custom_registration_function();
     return ob_get_clean();
-}
+}*/
