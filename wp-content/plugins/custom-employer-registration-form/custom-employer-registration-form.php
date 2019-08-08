0cd05ff8e981e3687167c5d0925b178aeea294cd
Non-admin action <nonadmin@example.com>
2019-08-08 04:19:28 +0100

    Amended employer reg form

--- wp-content/plugins/custom-employer-registration-form/custom-employer-registration-form.php
+++ wp-content/plugins/custom-employer-registration-form/custom-employer-registration-form.php
@@ -9,29 +9,44 @@
 
 function registration_form_fields() {
     ob_start(); ?>
-    <h3>Employer reg</h3>
 
     <form action="<?=$_SERVER['REQUEST_URI']?>" id="employer-registration" method="post">
         <input type="hidden" name="action" value="register_form">
         <fieldset>
             <div>
                 <label for="first-name">Nome<strong>*</strong></label>
-                <input type="text" name="first-name">
+                <input required
+                       type="text"
+                       name="first-name"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
             <div>
                 <label for="last-name">Cognome<strong>*</strong></label>
-                <input type="text" name="last-name">
+                <input required
+                       type="text"
+                       name="last-name"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
             <div>
                 <label for="role">Ruolo<strong>*</strong></label>
-                <input type="text" name="role">
+                <input required
+                       type="text"
+                       name="role"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
             <div>
                 <label for="company">Nome Azienda<strong>*</strong></label>
-                <input type="text" name="company">
+                <input required
+                       type="text"
+                       name="company"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
             <div>
@@ -41,23 +56,45 @@ function registration_form_fields() {
 
             <div>
                 <label for="phone-number">Numero di telefono<strong>*</strong></label>
-                <input type="text" name="phone-number">
+                <input required
+                       type="text"
+                       name="phone-number"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
             <div>
                 <label for="email">Email<strong>*</strong></label>
-                <input type="text" name="email">
+                <input required
+                       type="text"
+                       name="email"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
             <div>
                 <label for="password">Password<strong>*</strong></label>
-                <input type="text" name="password">
+                <input required
+                       type="text"
+                       name="password"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
 
-            <div>
+            <div style="margin-bottom:20px;">
+                <label for=">
                 <label for="confirm-password">Conferma password<strong>*</strong></label>
-                <input type="text" name="confirm-password">
+                <input required
+                       type="text"
+                       name="confirm-password"
+                       oninvalid="this.setCustomValidity('Campo obbligatorio')"
+                       onchange="this.setCustomValidity('')"/>
             </div>
+
+            <div style="margin-bottom:20px;">
+                <input required type="checkbox"> <a class="privacy-policy" href="#">Accetto l’informativa privacy</a>
+            </div>
+
 
             <div>
                 <?php do_action('add_new_user') ?>
@@ -67,6 +104,10 @@ function registration_form_fields() {
 
         </fieldset>
     </form>
+
+    <div class="privacy-policy-popup" style="height:200px; border:1px solid black; display:none";>
+        <p>L’informativa Privacy</p>
+    </div>
 
     <?php return ob_get_clean();
 }
@@ -107,15 +148,18 @@ function add_new_user() {
                 'last_name'		    => $user_last,
                 'user_registered'	=> date('Y-m-d H:i:s'),
                 'role'				=> 'subscriber',
-                'type'              => 'employer'
             ));
 
             $metas = array(
-                    'type'  => 'employer',
-                    'role'  => $role
+                    'type'          => 'employer',
+                    'role'          => $role,
+                    'company'       => $company,
+                    'headquarters'  => $headquarters
             );
 
-            //echo $new_user_ID; die();
+            foreach ($metas as $key => $value) {
+                update_user_meta($new_user_ID, $key, $value);
+            }
 
 
         } else {
@@ -124,201 +168,3 @@ function add_new_user() {
     }
 
 }
-
-/*if (isset($_POST['submit'])) {
-    add_new_user();
-}*/
-
-//var_dump($_POST); die();
-//add_action('register_form', 'add_new_user');
-
-/*function employer_registration_form($firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword) {
-    //var_dump(is_page(1645));
-    echo '
-    <style>
-    div {
-        margin-bottom:2px;
-    }
-
-    input{
-        margin-bottom:4px;
-    }
-    </style>
-    ';
-
-    echo '
-    <form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
-    <div>
-    <label for="first-name">Nome<strong>*</strong></label>
-    <input type="text" name="first-name" value="' . ( isset( $_POST['first-name'] ) ? $firstName : null ) . '">
-    </div>
-
-    <div>
-    <label for="last-name">Cognome<strong>*</strong></label>
-    <input type="text" name="last-name" value="' . ( isset( $_POST['last-name'] ) ? $lastName : null ) . '">
-    </div>
-
-    <div>
-    <label for="school">Scuola<strong>*</strong></label>
-    <input type="text" name="school" value="' . ( isset( $_POST['school'] ) ? $school : null ) . '">
-    </div>
-
-    <div>
-    <label for="course">Corso studi<strong>*</strong></label>
-    <input type="text" name="course" value="' . ( isset( $_POST['course'] ) ? $course : null ) . '">
-    </div>
-
-    <div>
-    <label for="graduation-year">Anno conseguimento titolo<strong></strong></label>
-    <input type="text" name="graduation-year" value="' . ( isset( $_POST['graduation-year'] ) ? $graduationYear : null ) . '">
-    </div>
-
-
-    <div>
-    <label for="email">Email<strong>*</strong></label>
-    <input type="text" name="email" value="' . ( isset( $_POST['email'] ) ? $email : null ) . '">
-    </div>
-
-    <div>
-    <label for="password">Password<strong>*</strong></label>
-    <input type="text" name="password" value="' . ( isset( $_POST['password'] ) ? $password : null ) . '">
-    </div>
-
-    <div>
-    <label for="confirm-password">Conferma password<strong>*</strong></label>
-    <input type="text" name="confirm-password" value="' . ( isset( $_POST['confirm-password'] ) ? $confirmPassword : null ) . '">
-    </div>
-
-
-    <input type="submit" name="submit" value="Registrati"/>
-    </form>
-    ';
-}
-
-
-function registration_validation($firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword) {
-
-    global $reg_errors;
-    $reg_errors = new WP_Error();
-
-    $mandatoryFields = [
-        $firstName,
-        $lastName,
-        $school,
-        $course,
-        $email,
-        $password
-    ];
-
-    foreach ($mandatoryFields as $mandatory) {
-        if (empty($mandatory)) {
-            $reg_errors->add('field', 'Required form field is missing');
-        }
-    }
-
-    if ( is_wp_error( $reg_errors ) ) {
-
-        foreach ( $reg_errors->get_error_messages() as $error ) {
-
-            echo '<div>';
-            echo '<strong>ERROR</strong>:';
-            echo $error . '<br/>';
-            echo '</div>';
-
-        }
-
-    }
-}
-
-
-function custom_registration_function() {
-    if ( isset($_POST['submit'] ) ) {
-        registration_validation(
-            $_POST['first-name'],
-            $_POST['last-name'],
-            $_POST['school'],
-            $_POST['course'],
-            $_POST['graduation-year'],
-            $_POST['email'],
-            $_POST['password'],
-            $_POST['confirm-password']
-        );
-
-        // sanitize user form input
-        global $firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword;
-        $firstName = sanitize_user( $_POST['first-name'] );
-        $lastName = sanitize_user( $_POST['last-name'] );
-        $school = sanitize_text_field( $_POST['school'] );
-        $course = sanitize_text_field( $_POST['password'] );
-        $graduationYear = sanitize_text_field( $_POST['graduation-year']);
-        $email = sanitize_email( $_POST['email'] );
-        $password = esc_attr( $_POST['password'] );
-        $confirmPassword = esc_attr( $_POST['confirm-password'] );
-
-        // call @function complete_registration to create the user
-        // only when no WP_error is found
-        complete_registration(
-            $firstName,
-            $lastName,
-            $school,
-            $course,
-            $graduationYear,
-            $email,
-            $password,
-            $confirmPassword
-        );
-    }
-
-    employer_registration_form(
-        $firstName,
-        $lastName,
-        $school,
-        $course,
-        $graduationYear,
-        $email,
-        $password,
-        $confirmPassword
-
-    );
-}
-
-function complete_registration() {
-    global $reg_errors, $firstName, $lastName, $school, $course, $graduationYear, $email, $password, $confirmPassword;
-
-    if ( 1 > count( $reg_errors->get_error_messages() ) ) {
-        $userdata = array(
-            'user_login'    => $firstName . '' . $lastName,
-            'user_email'    =>   $email,
-            'user_pass'     =>   $password,
-            'first_name'    =>   $firstName,
-            'last_name'     =>   $lastName,
-        );
-        $userID = wp_insert_user( $userdata );
-
-        $metas = array(
-            'type' => 'student',
-            'school' => $school,
-            'course' => $course,
-            'graduation_year' => $graduationYear
-        );
-
-
-        // Put an update condition in here too, in case the user exists?
-        foreach ($metas as $key => $value) {
-            update_user_meta($userID, $key, $value);
-        }
-        //$userMeta = add_user_meta($user->ID, 'type', 'student');
-        //var_dump($user); die();
-        echo 'Registration complete. Goto <a href="' . get_site_url() . '/wp-login.php">login page</a>.';
-    }
-}
-
-// Register a new shortcode: [cr_custom_registration]
-add_shortcode( 'employer_registration_form', 'custom_registration_shortcode' );
-
-// The callback function that will replace [book]
-function custom_registration_shortcode() {
-    ob_start();
-    custom_registration_function();
-    return ob_get_clean();
-}*/
